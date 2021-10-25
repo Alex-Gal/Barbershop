da290e97a8a9cb1af099369fd1d18175bf658a13
Alexey Galashko <82465083+Alex-Gal@users.noreply.github.com>
2021-10-25 10:15:12 +0300

    Try to fix slider

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-console.clear();
-
-// *Для тех, кто сделал лендинг с барбершопом:
-// подключите slick slider(либо другой удобный) для блока с мастерами,
-// сделайте, к примеру, чтобы мастеров было 10
-// (данные о мастерах просто продублируйте)
-// console.log($(".block4__masters_name").eq(1).text());
-// $(".block4__masters_box").slick();
 
 $(".slider__item").eq(0).clone().appendTo(".slider");
 $(".slider__item").eq(1).clone().appendTo(".slider");
@@ -19,10 +11,4 @@ $(".slider").slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 3
-  // responsive: [
-  //   {
-  //     breakpoint: 992,
-  //     settings: "unslick"
-  //   }
-  // ]
 });
