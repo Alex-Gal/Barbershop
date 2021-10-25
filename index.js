f6d42b5d9029cdce26764d5e93697740ce3addb2
Alexey Galashko <82465083+Alex-Gal@users.noreply.github.com>
2021-10-25 14:10:21 +0300

    Another try to fix slider and reduce imgs

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+$(document).ready(function () {
+  $(".slider__item").eq(0).clone().appendTo(".slider");
+  $(".slider__item").eq(1).clone().appendTo(".slider");
+  $(".slider__item").eq(2).clone().appendTo(".slider");
+  $(".slider__item").eq(0).clone().appendTo(".slider");
+  $(".slider__item").eq(1).clone().appendTo(".slider");
+  $(".slider__item").eq(2).clone().appendTo(".slider");
+  $(".slider__item").eq(0).clone().appendTo(".slider");
 
-$(".slider__item").eq(0).clone().appendTo(".slider");
-$(".slider__item").eq(1).clone().appendTo(".slider");
-$(".slider__item").eq(2).clone().appendTo(".slider");
-$(".slider__item").eq(0).clone().appendTo(".slider");
-$(".slider__item").eq(1).clone().appendTo(".slider");
-$(".slider__item").eq(2).clone().appendTo(".slider");
-$(".slider__item").eq(0).clone().appendTo(".slider");
-
-$(".slider").slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
+  $(".slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+      // responsive: [
+      //   {
+      //     breakpoint: 992,
+      //     settings: "unslick"
+      //   }
+      // ]
+  });
 });
+
+
